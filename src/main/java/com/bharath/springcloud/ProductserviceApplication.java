663b47ea09c88ac7c9f3e9597c79334a6ce8457c
nagarajan Pandian <nagarajan.pandiarajan@gmail.com>
2023-09-17 22:09:52 +0530

    changes to ProductserviceApplication

--- src/main/java/com/bharath/springcloud/ProductserviceApplication.java
+++ src/main/java/com/bharath/springcloud/ProductserviceApplication.java
@@ -15,6 +15,7 @@ public class ProductserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductserviceApplication.class, args);
+		//New changes to product repo
 	}
 
 }
